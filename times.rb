8edb9df40cb18f172d0b1ae6d4dd9fea8e0f8544
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-24 14:03:02 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -2,12 +2,4 @@
 	#your code here
 #end
 
-jewels_in_bag = 200
-
-5.times do 
-  puts "Hide 20 jewels in a safe location"
-  jewels_in_bag -= 20
-end 
-
-puts "I have #{jewels_in_bag} left to hide"
 
