5bcc8728c092d7c46498231681f624dc69c08118
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-24 13:58:48 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,13 @@
-def using_times
+#def using_times
 	#your code here
-end
+#end
 
+jewels_in_bag = 200
 
+5.times do 
+  puts "Hide 20 jewels in a safe location"
+  jewels_in_bag -= 20
+end 
 
+puts "I have #{jewels_in_bag} left to hide"
 
